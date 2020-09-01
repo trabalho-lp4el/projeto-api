a1d4ce33bdcb9030f96d58a3f3f61ab679b6f206
Calebe <calebe.fazzani.facul@gmail.com>
2020-09-01 01:43:06 -0300

    Adição de controller exemplo

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace projeto_api.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using projeto_api.Models;
 
 namespace projeto_api
 {
@@ -25,6 +27,7 @@ namespace projeto_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
